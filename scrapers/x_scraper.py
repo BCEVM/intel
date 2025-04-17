7795430d47bb4f9a7453708ed7d252e27ee25a05
Bcevm <arab.bnn@protonmail.com>
2025-04-17 16:34:13 +0700

    Create x_scraper.py

---
+++ scrapers/x_scraper.py
@@ -0,0 +1,16 @@
+# scrapers/x_scraper.py
+import snscrape.modules.twitter as sntwitter
+
+def scrape_x(keyword, limit=50):
+    results = []
+    for tweet in sntwitter.TwitterSearchScraper(f'{keyword}').get_items():
+        if len(results) >= limit:
+            break
+        results.append({
+            "platform": "X",
+            "user": tweet.user.username,
+            "text": tweet.content,
+            "timestamp": str(tweet.date),
+            "link": f"https://x.com/{tweet.user.username}/status/{tweet.id}"
+        })
+    return results
